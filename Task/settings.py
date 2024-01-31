84d741ed542da0cf68a69c481c7eb9e38b882066
sumeshjr <126143658+sumeshjr@users.noreply.github.com>
2024-01-31 21:32:09 +0530

    Update settings.py

--- Task/settings.py
+++ Task/settings.py
@@ -122,7 +122,7 @@ import os
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'TaskApp\static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
